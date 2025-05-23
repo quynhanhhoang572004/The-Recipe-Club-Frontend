fa51217ae3dc4f839f61e4a09f3e8fe2e72a366a
Hoàng Ngọc Quỳnh Anh <137272391+quynhanhhoang572004@users.noreply.github.com>
2025-05-23 08:51:54 +0700

    Update sign-up-page.tsx

--- src/pages/auth/sign-up-page.tsx
+++ src/pages/auth/sign-up-page.tsx
@@ -4,34 +4,9 @@ import { Box, Card } from "@mui/material";
 
 const signUpPage = () => {
   return (
-    <Box
-      sx={{
-        // minHeight: "100vh",
-        display: "flex",
-        justifyContent: "end",
-        alignItems: "center",
-        backgroundImage: `url(images/sign-up-background.png)`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "70%",
-        backgroundPosition: "left center",
-        position: "relative",
-        padding: "5rem",
-        margin: 0,
-
-        "&::before": {
-          content: '""', 
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-
-          zIndex: 1,
-        },
-      }}
-    >
-      <Card
-        elevation={12}
+    <>
+      <SeoMetaTags title="Sign up" />
+      <Box
         sx={{
           minHeight: "100vh",
           display: "flex",
@@ -43,7 +18,7 @@ const signUpPage = () => {
           position: "relative",
           padding: "5rem",
         }}
-      ></Card>
+      >
         <Card
           elevation={12}
           sx={{
@@ -54,7 +29,7 @@ const signUpPage = () => {
           <SignUpForm />
         </Card>
       </Box>
-
+    </>
   );
 };
 export default signUpPage;
